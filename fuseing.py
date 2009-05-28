ac8d6b1c69fc6d665468a290a02d8f1574394292
Andreas Zwinkau <beza1e1@web.de>
2009-05-28 17:33:34 +0200

    file creation works

--- fuseing.py
+++ fuseing.py
@@ -1,157 +1,51 @@
 import fuse, stat
 fuse.fuse_python_api = (0, 2)
 
-class MyStat(fuse.Stat):
-	def __init__(self):
-		self.st_mode = stat.S_IFDIR | 0755
-		self.st_ino = 0
-		self.st_dev = 0
-		self.st_nlink = 2
-		self.st_uid = 0
-		self.st_gid = 0
-		self.st_size = 4096
-		self.st_atime = 0
-		self.st_mtime = 0
-		self.st_ctime = 0
-
-
 class MyFuseFS(fuse.Fuse):
 	def __init__(self, fs):
 		print "my fuse fs initialized"
 		fuse.Fuse.__init__(self)
 		self._fs = fs
-
+	
 	def read(self, path, length, offset, fh=None):
 		"""read data from a file"""
-		pass
+		return self._fs.read(path, length, offset)
 
 	def write(self, path, buf, offset, fh=None):
-		"""write data to a file"""
-		pass
+		"""write data to a file, return amount of bytes written"""
+		return self._fs.write(path, buf, offset)
 
 	def readdir(self, path, offset, fh=None):
-		print "readdir", path, offset
-		for file in self._fs.get_directory(path):
-			yield fuse.Direntry(file)
+		return self._fs.readdir(path)
 
 	def getattr(self, path):
-		return MyStat()
-
-	def mknod(self, path, attrs):
-		pass
-
-	def mkdir(self, path):
-		pass
-
-	def readlink(self, path):
-		pass
-
-	def unlink(self, path):
-		pass
+		return self._fs.getattr(path)
 
 	def rmdir(self, path):
-		pass
+		self._fs.rmdir(path)
 
-	def symlink(self, path, target):
-		pass
-
-	def rename(self, path, new):
-		pass
-
-	def link(self, path, target):
-		pass
-
-	def chmod(self, path, mod):
-		pass
-
-	def chown(self, path, owner):
-		pass
-
-	def truncate(self, path, length):
-		pass
-
-	def statfs(self, path):
-		pass
-
-	def getxattr(self, path, attr, rest):
-		print "getxattr rest:", rest
-
-	def listxattr(self, path):
-		pass
-
-	def setxattr(self, path, attr, value):
-		pass
-
-	def removexattr(self, path, attr):
-		pass
-
-	def utimens(self, path, ts_acc, ts_mod):
-		pass
-
-	def bmap(self, path):
-		pass
-
-	### optional methods
-
-	def open(self, path, flags):
-		"""open a file"""
-		pass
-
-	def create(self, path, mode):
+	def create(self, path, mode, dev):
 		"""create a file"""
-		pass
-
-	def fsync(self, path, fdatasync, fh=None):
-		pass
-
-	def release(self, path, fh=None):
-		pass
-
-	def flush(self, path, fh=None):
-		"""clear all buffers, finish all pending operations"""
-		pass
-
-	def fgetattr(path, fh=None):
-		pass
-
-	def ftruncate(self, path, len, fh=None):
-		"""truncate file at position len"""
-		pass
-
-	def lock(self):
-		pass
-
-	def opendir(self, path, fh=None):
-		"""open a directory"""
-		# optional
-		# may return a file handle
-		pass
-
-	def fsyncdir(self, path, fh=None):
-		# optional
-		# fh may origin from opendir call
-		pass
-
-	def releasedir(self, path):
-		print "release dir", path
-		pass
-
-	def access(self, path):
-		pass
+		return self._fs.create(path, mode, dev)
 
 	def fsinit(self):
 		"""start file system"""
-		pass
+		print "fsinit"
+		self.__getattr__ = self.__getattr
 
 	def fsdestroy(self):
 		"""stop file system"""
-		pass
+		print "fsdestroy"
+		del self.__getattr__
 
-
+	def __getattr(self, attr):
+		print "fuse fs proxies", attr
+		return getattr(self._fs, attr)
 
 if __name__ == "__main__":
 	from kvfs import KVFS
-	core = KVFS(dict())
+	kv = dict()
+	core = KVFS(kv)
 
 	import sys
 	if len(sys.argv) < 2:
@@ -160,3 +54,4 @@ if __name__ == "__main__":
 	fs = MyFuseFS(core)
 	fs.parse(sys.argv)
 	fs.main()
+	print kv
--- kvfs.py
+++ kvfs.py
@@ -1,3 +1,39 @@
+from blobtree import BlobTree
+import fuse
+import marshal
+import stat
+
+class _MetaData:
+	def __init__(self, str=None):
+		if str:
+			self.data = marshal.loads(str)
+		else:
+			self.data = {
+					"st_mode": stat.S_IFDIR | 0755,
+					"st_ino": 0,
+					"st_dev": 0,
+					"st_nlink": 2,
+					"st_uid": 0,
+					"st_gid": 0,
+					"st_size": 4096,
+					"st_atime": 0,
+					"st_mtime": 0,
+					"st_ctime": 0
+					}
+		self.__setattr__ = self.__setattr
+	def __setattr(self, attr, value):
+		self.data[attr] = value
+	def __getattr__(self, attr):
+		return self.data[attr]
+	def __str__(self):
+		return marshal.dumps(self.data)
+	def __nonzero__(self):
+		return True
+
+def raise_io(number):
+	err = IOError()
+	err.errno = number
+	raise err
 
 class KVFS:
 	"""A Key-Value-File-System
@@ -5,14 +41,28 @@ class KVFS:
 	and implements a file system on top of it,
 	providing methods like open, close, read, write, ..."""
 	def __init__(self, kv_store):
-		self._kv = kv_store
+		self._bt = BlobTree(kv_store)
+		self.root_meta = _MetaData()
 
 	def get_directory(self, path):
-		return ['.', '..', 'blub']
+		return self._bt.list_dir(path)
 
-	def get_attribute(self, path):
+	def getattr(self, path):
 		"""Returns the attributes of the object at `path`."""
-		pass
+		if path == "/":
+			return self.root_meta
+		try:
+			return self._bt.get_meta_data(path)
+		except (KeyError, IndexError):
+			raise_io(fuse.ENOENT)
+
+	def setattr(self, path, meta):
+		if path == "/":
+			self.root_meta = meta
+		try:
+			self._bt.set_meta_data(path, meta)
+		except (KeyError, IndexError):
+			raise_io(fuse.ENOENT)
 
 	def getxattr(self, path):
 		"""Returns the extended attributes of the object at path. 
@@ -25,17 +75,27 @@ class KVFS:
 	def chown(self, path, uid, gid):
 		pass
 
-	def create(self, path, mode):
+	def create(self, path, mode, dev):
 		"""create a file"""
-		pass
+		m = _MetaData()
+		m.st_mode = mode
+		m.st_dev = dev
+		self._bt.create_data(path, m)
 
 	def mkdir(self, path, mode):
 		"""creates a directory"""
-		pass
+		self._bt.create_subtree(path)
 
-	def readdir(self, path, fh):
+	def readdir(self, path, fh=None):
 		"""read contents of a directory"""
-		pass
+		try:
+			files = self._bt.list_dir(path)
+		except KeyError:
+			raise_io(fuse.ENOENT)
+		yield fuse.Direntry('.')
+		yield fuse.Direntry('..')
+		for f in files:
+			yield fuse.Direntry(f)
 
 	def readlink(self, path):
 		"""Resolves a symbolic link"""
@@ -62,11 +122,15 @@ class KVFS:
 
 	def read(self, path, length, offset, fh=None):
 		"""read data from a file"""
-		pass
+		data = self._bt.get_data(path)
+		return data[offset:offset+length]
 
 	def write(self, path, buf, offset, fh=None):
 		"""write data to a file"""
-		pass
+		data = self._bt.get_data(path)
+		data = data[:offset] + buf + data[offset+len(buf):]
+		self._bt.set_data(path, data)
+		return len(buf)
 
 	def flush(self, path, fh=None):
 		"""clear all buffers, finish all pending operations"""
