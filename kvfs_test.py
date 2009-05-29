85309b6806bf23f66fa0ddbec0cc587c2a3f4097
Andreas Zwinkau <beza1e1@web.de>
2009-05-29 09:18:52 +0200

    initial kvfs unit tests

---
+++ kvfs_test.py
@@ -0,0 +1,20 @@
+from kvfs import KVFS
+import stat
+
+def test_basic():
+	K = KVFS(dict())
+	K.getattr("/")
+	K.readdir("/")
+
+def test_basic_file():
+	K = KVFS(dict())
+	K.create("/blub", stat.S_IFREG, 2, 3, 4)
+	K.getattr("/blub")
+	K.remove("/blub")
+
+def test_basic_dir():
+	K = KVFS(dict())
+	K.mkdir("/bla", stat.S_IFDIR, 2, 3)
+	K.getattr("/bla")
+	K.readdir("/bla")
+	K.remove("/bla")
