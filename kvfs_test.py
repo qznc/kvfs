5dedfc43b187293cd343c6aea3e3f05135ccfb3d
Andreas Zwinkau <beza1e1@samwise.local>
2009-05-30 10:58:50 +0200

    more asserts in unit tests

--- kvfs_test.py
+++ kvfs_test.py
@@ -1,31 +1,38 @@
 from kvfs import KVFS
 import stat
 
-def test_basic():
+def test_basic_root():
+	"""testing basic root properties"""
 	K = KVFS(dict())
-	K.getattr("/")
-	K.readdir("/")
+	attr = K.getattr("/")
+	assert stat.S_ISDIR(attr['st_mode'])
+	dir = K.readdir("/")
+	assert '.' in dir
+	assert '..' in dir
 
 def test_basic_file():
+	"""testing basic file properties"""
 	K = KVFS(dict())
 	K.create("/blub")
+	assert "blub" in K.readdir("/")
 	attr = K.getattr("/blub")
 	assert stat.S_ISREG(attr['st_mode'])
 	K.remove("/blub")
 	K.flush()
 
 def test_basic_dir():
+	"""testing basic directory properties"""
 	K = KVFS(dict())
 	K.mkdir("/bla")
-	attr = K.getattr("/bla")
-	assert stat.S_ISDIR(attr['st_mode'])
+	assert "bla" in K.readdir("/")
+	assert stat.S_ISDIR(K.getattr("/bla")['st_mode'])
 	dir = list(K.readdir("/bla"))
 	assert '.' in dir
 	assert '..' in dir
 	K.remove("/bla")
 	K.flush("/")
 
-def test_root():
+def test_used_root():
 	K = KVFS(dict())
 	attr = K.getattr("/")
 	assert stat.S_ISDIR(attr['st_mode'])
